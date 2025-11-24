f9c425544f5dc26b1a072a87bdb952d6ef742b97
Forrest Miller <forrestmillerj@gmail.com>
2025-11-24 11:42:16 -0600

    homelab oidc bucket

--- infra/modules/aws/s3/s3.tf
+++ infra/modules/aws/s3/s3.tf
@@ -10,3 +10,38 @@ resource "aws_s3_bucket" "homelab" {
   }
 }
 
+resource "aws_s3_bucket" "talos_irsa" {
+  bucket = "${var.namespace}-${var.environment}-homelab-oidc"
+    
+  tags = {
+    Owner       = "Forrest Miller"
+    Email       = "forrestmillerj@gmail.com"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "talos_irsa" {
+  bucket = aws_s3_bucket.talos_irsa.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "talos_irsa" {
+  bucket = aws_s3_bucket.talos_irsa.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "talos_irsa" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.talos_irsa,
+    aws_s3_bucket_public_access_block.talos_irsa,
+  ]
+
+  bucket = aws_s3_bucket.talos_irsa.id
+  acl    = "public-read"
+}
