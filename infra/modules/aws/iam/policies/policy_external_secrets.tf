8b283717f802a1b1343dc840957c4f8de4e3a63f
Forrest Miller <forrestmillerj@gmail.com>
2025-11-24 12:25:10 -0600

    fix deprecated attr

--- infra/modules/aws/iam/policies/policy_external_secrets.tf
+++ infra/modules/aws/iam/policies/policy_external_secrets.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "external_secrets" {
     ]
 
     resources = [
-      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:*"
+      "arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:*"
     ]
   }
 
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "external_secrets" {
     ]
 
     resources = [
-      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:*"
+      "arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:*"
     ]
   }
 }
